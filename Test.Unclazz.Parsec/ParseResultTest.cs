14d4353dc49fbea7b8ae743e3e37f15f14f81163
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-12 16:56:56 +0900

    ParseResultの実装を追加

---
+++ Test.Unclazz.Parsec/ParseResultTest.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unclazz.Parsec;
+
+namespace Test.Unclazz.Parsec
+{
+    [TestFixture]
+    public class ParseResultTest
+    {
+        [Test]
+        public void Successful_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            var s0 = r0.Successful;
+            var s1 = r1.Successful;
+
+            // Assert
+            Assert.That(s0, Is.True);
+            Assert.That(s1, Is.False);
+        }
+        [Test]
+        public void Value_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            // Assert
+            try
+            {
+                var v0 = r0.Value;
+                Assert.That(v0, Is.EqualTo(123));
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.Fail();
+            }
+            try
+            {
+                var v1 = r1.Value;
+                Assert.Fail();
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.That(r1.Message, Is.EqualTo("123"));
+            }
+        }
+        [Test]
+        public void Message_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            // Assert
+            try
+            {
+                var v0 = r0.Message;
+                Assert.Fail();
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.That(r0.Value, Is.EqualTo(123));
+            }
+            try
+            {
+                var v1 = r1.Message;
+                Assert.That(v1, Is.EqualTo("123"));
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void FirstOrDefault_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            var v0 = r0.FirstOrDefault();
+            var v1 = r1.FirstOrDefault();
+
+            // Assert
+            Assert.That(v0, Is.EqualTo(123));
+            Assert.That(v1, Is.EqualTo(0));
+        }
+        [Test]
+        public void SingleOrDefault_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            var v0 = r0.SingleOrDefault();
+            var v1 = r1.SingleOrDefault();
+
+            // Assert
+            Assert.That(v0, Is.EqualTo(123));
+            Assert.That(v1, Is.EqualTo(0));
+        }
+        [Test]
+        public void IfSuccessful_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            // Assert
+            r0.IfSuccessful(v => Assert.That(v, Is.EqualTo(123)));
+            r1.IfSuccessful(v => Assert.Fail());
+        }
+        [Test]
+        public void IfSuccessful_Case2()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            // Assert
+            r0.IfSuccessful(v => Assert.That(v, Is.EqualTo(123)), v => Assert.Fail());
+            r1.IfSuccessful(v => Assert.Fail(), v => Assert.That(v, Is.EqualTo("123")));
+        }
+        [Test]
+        public void IfFailed_Case1()
+        {
+            // Arrange
+            var r0 = ParseResult.OfSuccess(CharacterPosition.StartOfFile, 123);
+            var r1 = ParseResult.OfFailure<int>(CharacterPosition.StartOfFile, "123");
+
+            // Act
+            // Assert
+            r0.IfFailed(v => Assert.Fail());
+            r1.IfFailed(v => Assert.That(v, Is.EqualTo("123")));
+        }
+    }
+}
--- Unclazz.Parsec/ParseResult.cs
+++ Unclazz.Parsec/ParseResult.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +7,187 @@ using System.Threading.Tasks;
 
 namespace Unclazz.Parsec
 {
-    public abstract class ParseResult<T>
+    /// <summary>
+    /// <see cref="ParseResult{T}"/>のコンパニオン・オブジェクトです。
+    /// <see cref="ParseResult{T}"/>のインスタンスを生成するためのユーティリティとして機能します。
+    /// </summary>
+    public static class ParseResult
     {
-        internal ParseResult()
+        /// <summary>
+        /// パース成功を表す<see cref="ParseResult{T}"/>インスタンスを返します。
+        /// </summary>
+        /// <typeparam name="T">パース結果の型</typeparam>
+        /// <param name="p">パース開始時の文字位置</param>
+        /// <param name="v">パース結果の値</param>
+        /// <returns><see cref="ParseResult{T}"/>インスタンス</returns>
+        public static ParseResult<T> OfSuccess<T>(CharacterPosition p, T v)
         {
+            return new ParseResult<T>(true, p, v, null);
+        }
+        /// <summary>
+        /// パース失敗を表す<see cref="ParseResult{T}"/>インスタンスを返します。
+        /// </summary>
+        /// <typeparam name="T">パース結果の型</typeparam>
+        /// <param name="p">パース開始時の文字位置</param>
+        /// <param name="m">パース失敗の理由を示すメッセージ</param>
+        /// <returns><see cref="ParseResult{T}"/>インスタンス</returns>
+        public static ParseResult<T> OfFailure<T>(CharacterPosition p, string m)
+        {
+            return new ParseResult<T>(false, p, default(T), m);
+        }
+    }
+
+    /// <summary>
+    /// パース結果を表すクラスです。
+    /// インスタンスはコンパニオン・オブジェクト<see cref="ParseResult"/>の提供する静的ファクトリーメソッドにより得られます。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class ParseResult<T> : IEnumerable<T>
+    {
+        static readonly IEnumerable<T> _empty = new T[0];
+
+        internal ParseResult(bool s, CharacterPosition p, T v, string m)
+        {
+            if (!s && m == null) throw new ArgumentNullException(nameof(m));
+
+            if (s) _value = v;
+            else _message = m;
+
+            Successful = s;
+            Position = p;
         }
 
-        public T Value { get; }
-        public int Position { get; }
-        public int LinePosition { get; }
-        public int ColumnPosition { get; }
-        public string Message { get; }
-        bool Success => Position > 0;
+        T _value;
+        string _message;
+
+        /// <summary>
+        /// パース結果の値です。
+        /// パースが失敗している場合は例外をスローします。
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                if (Successful) return _value;
+                else throw new InvalidOperationException("No value.");
+            }
+        }
+        /// <summary>
+        /// パース開始時の文字位置です。
+        /// </summary>
+        public CharacterPosition Position { get; }
+        /// <summary>
+        /// パース失敗の理由を示すメッセージです。
+        /// パースが成功している場合は例外をスローします。
+        /// </summary>
+        public string Message
+        {
+            get
+            {
+                if (Successful)
+                {
+                    throw new InvalidOperationException("No message.");
+                }
+                else
+                {
+                    return _message;
+                }
+            }
+        }
+        /// <summary>
+        /// パースが成功している場合<c>true</c>です。
+        /// </summary>
+        public bool Successful { get; }
+
+        /// <summary>
+        /// パースが成功している場合は引数で指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="act">アクション</param>
+        public void IfSuccessful(Action<T> act)
+        {
+            if (Successful) (act ?? throw new ArgumentNullException(nameof(act)))(Value);
+        }
+        /// <summary>
+        /// パースが成功している場合は引数で指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="act">アクション</param>
+        public void IfSuccessful(Action<CharacterPosition, T> act)
+        {
+            if (Successful) (act ?? throw new ArgumentNullException(nameof(act)))(Position, Value);
+        }
+        /// <summary>
+        /// パースが成功している場合は第1引数で指定されたアクションを実行します。
+        /// さもなくば第2引数で指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="act">成功している場合に実行されるアクション</param>
+        /// <param name="orElse">失敗している場合に実行されるアクション</param>
+        public void IfSuccessful(Action<T> act, Action<string> orElse)
+        {
+            if (Successful) (act ?? throw new ArgumentNullException(nameof(act)))(Value);
+            else (orElse ?? throw new ArgumentNullException(nameof(orElse)))(Message);
+        }
+        /// <summary>
+        /// パースが成功している場合は第1引数で指定されたアクションを実行します。
+        /// さもなくば第2引数で指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="act">成功している場合に実行されるアクション</param>
+        /// <param name="orElse">失敗している場合に実行されるアクション</param>
+        public void IfSuccessful(Action<CharacterPosition, T> act, Action<CharacterPosition, string> orElse)
+        {
+            if (Successful) (act ?? throw new ArgumentNullException(nameof(act)))(Position, Value);
+            else (orElse ?? throw new ArgumentNullException(nameof(orElse)))(Position, Message);
+        }
+        /// <summary>
+        /// パースが失敗している場合は引数で指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="act">アクション</param>
+        public void IfFailed(Action<string> act)
+        {
+            if (!Successful) (act ?? throw new ArgumentNullException(nameof(act)))(Message);
+        }
+        /// <summary>
+        /// パースが失敗している場合は引数で指定されたアクションを実行します。
+        /// </summary>
+        /// <param name="act">アクション</param>
+        public void IfFailed(Action<CharacterPosition, string> act)
+        {
+            if (!Successful) (act ?? throw new ArgumentNullException(nameof(act)))(Position, Message);
+        }
+        /// <summary>
+        /// 列挙子を返します。
+        /// パースが成功している場合、列挙子は唯一の要素としてパース結果の値を列挙します。
+        /// パースが失敗している場合、列挙子はいかなる値も列挙しません。
+        /// </summary>
+        /// <returns>列挙子</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (Successful)
+            {
+                return ((IEnumerable<T>)new T[] { Value }).GetEnumerator();
+            }
+            else
+            {
+                return _empty.GetEnumerator();
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        /// <summary>
+        /// このインスタンスの文字列表現を返します。
+        /// </summary>
+        /// <returns>文字列表現</returns>
+        public override string ToString()
+        {
+            if (Successful)
+            {
+                return string.Format("ParseResult(Successful = {0}, Positon = {1}, Value = {2})", true, Position, Value);
+            }
+            else
+            {
+                return string.Format("ParseResult(Successful = {0}, Positon = {1}, Message = {2})", false, Position, Message);
+            }
+        }
     }
 }
