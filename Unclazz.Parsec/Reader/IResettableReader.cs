afffa7dbc6ccc8805629d13c6aa39eaf2cbc63b7
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-13 20:52:59 +0900

    IResettableReaderのドキュメンテーションXMLを加筆修正

--- Unclazz.Parsec/Reader/IResettableReader.cs
+++ Unclazz.Parsec/Reader/IResettableReader.cs
@@ -9,9 +9,19 @@ namespace Unclazz.Parsec.Reader
     /// <summary>
     /// リセット機能を持つ<see cref="ITextReader"/>です。
     /// <para>
-    /// あらかじめ<see cref="Mark"/>により文字位置に印をつけておくことで、
+    /// あらかじめ<see cref="Mark"/>により文字位置を記録しておくことで、
     /// <see cref="ITextReader.Read"/>により文字位置を先に進めたあとで
     /// <see cref="Reset"/>により文字位置を元に戻すことができます。
+    /// </para>
+    /// <para>
+    /// <see cref="Mark"/>を呼び出すたびにその時点での文字位置が記録されます。
+    /// <see cref="Mark"/>を呼び出した後、例え文字位置が1文字も前進していない場合でも<see cref="Mark"/>を再度呼び出すと、
+    /// その時の文字位置が独立した記録として残ります。
+    /// </para>
+    /// <para>
+    /// <see cref="Unmark"/>を呼び出すと直近の<see cref="Mark"/>呼び出しで行われた文字位置の記録が削除されます。
+    /// <see cref="Mark"/>により残された文字位置の記録すべてを削除するには、
+    /// <see cref="Mark"/>と同じ回数だけ<see cref="Unmark"/>を呼び出します。
     /// </para>
     /// </summary>
     public interface IResettableReader : ITextReader
@@ -22,12 +32,12 @@ namespace Unclazz.Parsec.Reader
         void Mark();
         /// <summary>
         /// <see cref="Mark"/>により設定された印をはずします。
-        /// まだ<see cref="Mark"/>が呼び出された実績がない場合、このメソッドは何も行わず、例外も発生しません。
+        /// まだ<see cref="Mark"/>が呼び出された実績がない場合、このメソッドは何も行わず、例外もスローしません。
         /// </summary>
         void Unmark();
         /// <summary>
         /// あらかじめ印をつけておいた文字位置に戻ります。
-        /// このメソッドを呼び出しても印は外されないので、何度でも<see cref="Reset"/>を行うことができます。
+        /// まだ<see cref="Mark"/>が呼び出された実績がない場合、このメソッドは何も行わず、例外もスローしません。
         /// </summary>
         void Reset();
     }
