7a6e65706d1008cb42fd3b7897849acfe63db314
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-15 02:02:03 +0900

    WordParserのテストケースを追加

--- Test.Unclazz.Parsec/WordParserTest.cs
+++ Test.Unclazz.Parsec/WordParserTest.cs
@@ -28,6 +28,56 @@ namespace Test.Unclazz.Parsec
 
             // Assert
             Assert.That(result.Successful, Is.EqualTo(expectedResult));
+            result.IfSuccessful((pos, cap) =>
+            {
+                Assert.That(pos.Index, Is.EqualTo(0));
+                Assert.That(cap.HasValue, Is.False);
+                Assert.That(() => cap.Value, Throws.InstanceOf<InvalidOperationException>());
+            });
+        }
+        [TestCase("0123456789X", "1", true)]
+        [TestCase("0123456789X", "12", true)]
+        [TestCase("0123456789X", "01", false)]
+        public void Parse_Case2(string text, string word, bool expectedResult)
+        {
+            // Arrange
+            var input = ParserInput.FromString(text);
+            var parser = Parser.Word(word);
+            input.Read();
+
+            // Act
+            var result = parser.Parse(input);
+
+            // Assert
+            Assert.That(result.Successful, Is.EqualTo(expectedResult));
+            result.IfSuccessful((pos, cap) =>
+            {
+                Assert.That(pos.Index, Is.EqualTo(1));
+                Assert.That(cap.HasValue, Is.False);
+                Assert.That(() => cap.Value, Throws.InstanceOf<InvalidOperationException>());
+            });
+        }
+        [TestCase("0123456789X", "1", true)]
+        [TestCase("0123456789X", "12", true)]
+        [TestCase("0123456789X", "01", false)]
+        public void Parse_Case3(string text, string word, bool expectedResult)
+        {
+            // Arrange
+            var input = ParserInput.FromString(text);
+            var parser = Parser.Word(word).Capture();
+            input.Read();
+
+            // Act
+            var result = parser.Parse(input);
+
+            // Assert
+            Assert.That(result.Successful, Is.EqualTo(expectedResult));
+            result.IfSuccessful((pos, cap) =>
+            {
+                Assert.That(pos.Index, Is.EqualTo(1));
+                Assert.That(cap.HasValue, Is.True);
+                Assert.That(cap.Value, Is.EqualTo(word));
+            });
         }
     }
 }
