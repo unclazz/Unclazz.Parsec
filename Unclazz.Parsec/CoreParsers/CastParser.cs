103c21994feba7f6c3f5499758a0a7d0e268e789
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-17 08:49:46 +0900

    CastParserを追加

---
+++ Unclazz.Parsec/CoreParsers/CastParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Unclazz.Parsec
+{
+    sealed class CastParser<T, U> : Parser<U>
+    {
+        internal CastParser(Parser<T> original)
+        {
+            _original = original ?? throw new ArgumentNullException(nameof(original));
+        }
+        readonly Parser<T> _original;
+        public override ParseResult<U> Parse(ParserInput input)
+        {
+            return _original.Parse(input).Cast<U>();
+        }
+        public override string ToString()
+        {
+            return string.Format("Cast({0}, type = {1})", _original,
+                ParsecUtility.TypeToString(typeof(U)));
+        }
+    }
+}
