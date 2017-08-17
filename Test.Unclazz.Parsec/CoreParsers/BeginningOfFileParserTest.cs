d89acff6606328225184cf426c189f27ed14218e
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-18 02:16:54 +0900

    BOF/EOF用のパーサーのテストを追加

---
+++ Test.Unclazz.Parsec/CoreParsers/BeginningOfFileParserTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Unclazz.Parsec;
+
+namespace Test.Unclazz.Parsec.CoreParsers
+{
+    [TestFixture]
+    public class BeginningOfFileParserTest
+    {
+        [Test]
+        public void Parse_Case1()
+        {
+            var input = ParserInput.FromString("hello");
+            var parser = Parser.BeginningOfFile;
+            Assert.That(parser.Parse(input).Successful, Is.True);
+        }
+        [Test]
+        public void Parse_Case2()
+        {
+            var input = ParserInput.FromString("hello");
+            var parser = Parser.BeginningOfFile;
+            input.Read();
+            Assert.That(parser.Parse(input).Successful, Is.False);
+        }
+        [Test]
+        public void Parse_Case3()
+        {
+            var input = ParserInput.FromString(string.Empty);
+            var parser = Parser.BeginningOfFile;
+            Assert.That(parser.Parse(input).Successful, Is.True);
+        }
+        [Test]
+        public void Parse_Case4()
+        {
+            var input = ParserInput.FromString("hello");
+            var parser = Parser.BeginningOfFile;
+            TestUtility.ConsumesAll(input);
+            Assert.That(parser.Parse(input).Successful, Is.False);
+        }
+    }
+}
---
+++ Test.Unclazz.Parsec/CoreParsers/EndOfFileParserTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unclazz.Parsec;
+
+namespace Test.Unclazz.Parsec.CoreParsers
+{
+    [TestFixture]
+    public class EndOfFileParserTest
+    {
+        [Test]
+        public void Parse_Case1()
+        {
+            var input = ParserInput.FromString("hello");
+            var parser = Parser.EndOfFile;
+            Assert.That(parser.Parse(input).Successful, Is.False);
+        }
+        [Test]
+        public void Parse_Case2()
+        {
+            var input = ParserInput.FromString("h");
+            var parser = Parser.EndOfFile;
+            Assert.That(parser.Parse(input).Successful, Is.False);
+        }
+        [Test]
+        public void Parse_Case3()
+        {
+            var input = ParserInput.FromString(string.Empty);
+            var parser = Parser.EndOfFile;
+            Assert.That(parser.Parse(input).Successful, Is.True);
+        }
+        [Test]
+        public void Parse_Case4()
+        {
+            var input = ParserInput.FromString("hello");
+            var parser = Parser.EndOfFile;
+            TestUtility.ConsumesAll(input);
+            Assert.That(parser.Parse(input).Successful, Is.True);
+        }
+    }
+}
