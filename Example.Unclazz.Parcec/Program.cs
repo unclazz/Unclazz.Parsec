4439126b82153b019148e1873bd512d577360723
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-18 02:19:59 +0900

    サンプルコードに演算子オーバーロードの使用例を導入

--- Example.Unclazz.Parcec/Program.cs
+++ Example.Unclazz.Parcec/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unclazz.Parsec;
+using static Unclazz.Parsec.Parser;
 
 namespace Example.Unclazz.Parcec
 {
@@ -12,14 +13,13 @@ namespace Example.Unclazz.Parcec
         static void Main(string[] args)
         {
             // 例：JavaScriptのIntegerリテラルを読み取るパーサー
-            var space = Parser.CharsWhileIn(" \r\n", min: 0);
-            var sign = Parser.CharIn("+-").OrNot();
-            var digits = Parser.CharsWhileIn("0123456789");
-            var integral = Parser.Char('0').Or(Parser.CharBetween('1', '9').Concat(digits));
-            var fractional = Parser.Char('.').Concat(digits);
-            var exponent = Parser.CharIn("eE").Concat(sign).Concat(digits);
-            var number = space.Concat(sign.Concat(integral, fractional.OrNot(),
-                exponent.OrNot()).Capture()).Concat(space).Map(double.Parse);
+            var space = CharsWhileIn(" \r\n", min: 0);
+            var sign = CharIn("+-").OrNot();
+            var digits = CharsWhileIn("0123456789");
+            var integral = Char('0') | (CharBetween('1', '9') & digits);
+            var fractional = Char('.') & digits;
+            var exponent = CharIn("eE") & (sign) & (digits);
+            var number = (space & (sign & integral & fractional.OrNot() & exponent.OrNot()).Capture() & space).Map(double.Parse);
 
             Console.WriteLine("input = {0}", args[0]);
             Console.WriteLine("parsed = {0}", number.Parse(args[0]));
