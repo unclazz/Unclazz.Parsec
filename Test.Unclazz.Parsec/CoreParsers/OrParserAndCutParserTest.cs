d9ff44756fba00bd1108f4ce50bbd1cf0853d1eb
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-19 00:02:16 +0900

    Or/Cutの関係性を考慮したテストケースを追加

---
+++ Test.Unclazz.Parsec/CoreParsers/OrParserAndCutParserTest.cs
@@ -0,0 +1,132 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unclazz.Parsec;
+using static Unclazz.Parsec.Parser;
+
+namespace Test.Unclazz.Parsec.CoreParsers
+{
+    [TestFixture]
+    public class OrParserAndCutParserTest
+    {
+        [Test]
+        public void Parse_Case1()
+        {
+            // Arrange
+            var p = Keyword("012A", 2) | Keyword("01B3") | Keyword("0C23");
+            ParserInput i = "0123";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'C'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case2()
+        {
+            // Arrange
+            var p = Keyword("012A", 2) | (Keyword("01B3") | Keyword("0C23"));
+            ParserInput i = "0123";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'A'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case3()
+        {
+            // Arrange
+            var p = Keyword("0123A", 3) | Keyword("012B4", 2) | Keyword("01C34", 1);
+            ParserInput i = "01234";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'C'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case4()
+        {
+            // Arrange
+            var p = Keyword("0123A", 3) | (Keyword("012B4", 2) | Keyword("01C34", 1));
+            ParserInput i = "01234";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'A'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case5()
+        {
+            // Arrange
+            var p = CutStringAt("012A", 2) | Keyword("01B3") | Keyword("0C23");
+            ParserInput i = "0123";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'C'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case6()
+        {
+            // Arrange
+            var p = CutStringAt("012A", 2) | (Keyword("01B3") | Keyword("0C23"));
+            ParserInput i = "0123";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'A'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case7()
+        {
+            // Arrange
+            var p = CutStringAt("0123A", 3) | CutStringAt("012B4", 2) | CutStringAt("01C34", 1);
+            ParserInput i = "01234";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'C'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        [Test]
+        public void Parse_Case8()
+        {
+            // Arrange
+            var p = CutStringAt("0123A", 3) | (CutStringAt("012B4", 2) | CutStringAt("01C34", 1));
+            ParserInput i = "01234";
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Message, Does.StartWith("expected 'A'"));
+            Assert.That(r.CanBacktrack, Is.True);
+        }
+        Parser<string> CutStringAt(string s, int cutIndex)
+        {
+            return Keyword(s.Substring(0, cutIndex)).Cut() & Keyword(s.Substring(cutIndex));
+        }
+    }
+}
