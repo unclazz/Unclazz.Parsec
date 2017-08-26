11da4a86d20afc4b5305c864bed3710f3ae2aed0
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-26 20:15:51 +0900

    DoubleParserのためのテストケースを追加

---
+++ Test.Unclazz.Parsec/CoreParsers/DoubleParserTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using Unclazz.Parsec;
+using static Unclazz.Parsec.Parsers;
+
+namespace Test.Unclazz.Parsec.CoreParsers
+{
+    [TestFixture]
+    public class DoubleParserTest
+    {
+        [Test]
+        public void Parse_Case1()
+        {
+            // Arrange
+            var kp = Keyword("012").Capture();
+            var dp = kp.Then(Char('3').Capture());
+
+            // Act
+            var res = dp.Parse("012XXXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.False);
+            Assert.That(() => res.Capture.Present, Throws.InstanceOf<InvalidOperationException>());
+        }
+        [Test]
+        public void Parse_Case2()
+        {
+            // Arrange
+            var kp = Keyword("012").Capture();
+            var dp = kp.Then(Char('3').Capture());
+
+            // Act
+            var res = dp.Parse("0123XXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.True);
+            Assert.That(res.Capture.Present, Is.True);
+            Assert.That(res.Capture.Value.Item1, Is.EqualTo("012"));
+            Assert.That(res.Capture.Value.Item2, Is.EqualTo("3"));
+        }
+        [Test]
+        public void Parse_Case3()
+        {
+            // Arrange
+            var kp = Keyword("012").Capture();
+            var dp = kp.Then(Char('3').Cast<string>());
+
+            // Act
+            var res = dp.Parse("0123XXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.True);
+            Assert.That(res.Capture.Present, Is.True);
+            Assert.That(res.Capture.Value.Item1, Is.EqualTo("012"));
+            Assert.That(res.Capture.Value.Item2, Is.Null);
+        }
+    }
+}
