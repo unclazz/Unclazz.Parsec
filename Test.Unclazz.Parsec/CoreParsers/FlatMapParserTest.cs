71164d25bf282a8ac4a391e9b92dac7703b788d8
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-08-30 09:04:26 +0900

    FlatMapParserとそのテストケースを追加

---
+++ Test.Unclazz.Parsec/CoreParsers/FlatMapParserTest.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using System;
+using Unclazz.Parsec;
+using static Unclazz.Parsec.Parsers;
+
+namespace Test.Unclazz.Parsec.CoreParsers
+{
+    [TestFixture]
+    public class FlatMapParserTest
+    {
+        [Test]
+        [Description("Parse - Case1 - パース失敗の場合")]
+        public void Parse_Case1()
+        {
+            // Arrange
+            var kp = Keyword("0123").Capture();
+            var mp = kp.FlatMap(a => Yield("abcd"));
+
+            // Act
+            var res = mp.Parse("012XXXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.False);
+            Assert.That(() => res.Capture.Present, Throws.InstanceOf<InvalidOperationException>());
+        }
+        [Test]
+        [Description("Parse - Case2 - パース成功 x キャプチャなしの場合")]
+        public void Parse_Case2()
+        {
+            // Arrange
+            var kp = Keyword("0123");
+            var mp = kp.FlatMap(a => Yield("abcd"));
+
+            // Act
+            var res = mp.Parse("0123XXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.True);
+            Assert.That(res.Capture.Present, Is.False);
+        }
+        [Test]
+        [Description("Parse - Case3 - パース成功 x キャプチャありの場合")]
+        public void Parse_Case3()
+        {
+            // Arrange
+            var kp = Keyword("0123");
+            var mp = kp.Capture().FlatMap(a => Yield("abcd"));
+
+            // Act
+            var res = mp.Parse("0123XXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.True);
+            Assert.That(res.Capture.Present, Is.True);
+            Assert.That(res.Capture.Value, Is.EqualTo("abcd"));
+        }
+        [Test]
+        [Description("Parse - Case4 - パース失敗 x 変換に失敗 x 例外スローNGの場合")]
+        public void Parse_Case4()
+        {
+            // Arrange
+            var kp = Keyword("0123");
+            var mp = kp.Capture().FlatMap(a => Yield(bool.Parse(a)));
+
+            // Act
+            var res = mp.Parse("0123XXXX");
+
+            // Assert
+            Assert.That(res.Successful, Is.False);
+            Assert.That(() => res.Capture.Present, Throws.InstanceOf<InvalidOperationException>());
+        }
+        [Test]
+        [Description("Parse - Case5 - パース失敗 x 変換に失敗 x 例外スローOKの場合")]
+        public void Parse_Case5()
+        {
+            // Arrange
+            var kp = Keyword("0123");
+            var mp = kp.Capture().FlatMap(a => Yield(bool.Parse(a)), canThrow: true);
+
+            // Act
+            // Assert
+            Assert.That(() => mp.Parse("0123XXXX"), Throws.InstanceOf<FormatException>());
+        }
+    }
+}
---
+++ Unclazz.Parsec/CoreParsers/FlatMapParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Unclazz.Parsec.CoreParsers
+{
+    sealed class FlatMapParser<T, U> : Parser<U>
+    {
+        internal FlatMapParser(IParserConfiguration conf, Parser<T> source, 
+            Func<T, Parser<U>> mapper, bool canThrow) : base(conf)
+        {
+            _source = source ?? throw new ArgumentNullException(nameof(source));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _canThrow = canThrow;
+        }
+
+        readonly Parser<T> _source;
+        readonly Func<T, Parser<U>> _mapper;
+        readonly bool _canThrow;
+
+        protected override ParseResult<U> DoParse(Reader input)
+        {
+            var r = _source.Parse(input);
+            try
+            {
+                if (r.Successful && r.Capture.Present)
+                {
+                    return _mapper(r.Capture.Value).Parse(input);
+                }
+                return r.Cast<U>();
+            }
+            catch (Exception ex)
+            {
+                if (_canThrow) throw;
+                return Failure(r.Position, ex.ToString(), r.CanBacktrack);
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("FlatMap({0}, transform = {1})",
+                _source, ParsecUtility.ObjectTypeToString(_mapper));
+        }
+    }
+}
