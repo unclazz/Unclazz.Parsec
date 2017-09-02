1d71ff225e4487f0ca39bd9f3035090de837d2a3
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-02 09:46:25 +0900

    Typed/UntypedParserにドキュメンテーションXMLを追加

--- Unclazz.Parsec/CoreParsers/TypedParser.cs
+++ Unclazz.Parsec/CoreParsers/TypedParser.cs
@@ -2,18 +2,26 @@
 
 namespace Unclazz.Parsec.CoreParsers
 {
-    sealed class TypedParser<T> : Parser<T>
+    /// <summary>
+    /// 結果型を持たないパーサーに結果型を付与するパーサーです。
+    /// コンストラクタに結果値として使用される値を指定しなかった場合、
+    /// パーサーが返す結果値は結果型<typeparamref name="TResult"/>のデフォルト値となります。
+    /// </summary>
+    /// <typeparam name="TResult">このパーサーの結果型</typeparam>
+    sealed class TypedParser<TResult> : Parser<TResult>
     {
-        internal TypedParser(Parser original) : this(original, default(T)) { }
-        internal TypedParser(Parser original, T value) : base(original.Configuration)
+        internal TypedParser(Parser original) : this(original, default(TResult)) { }
+        internal TypedParser(Parser original, TResult value) : base(original.Configuration)
         {
             _original = original ?? throw new ArgumentNullException(nameof(original));
             _value = value;
         }
         readonly Parser _original;
-        readonly T _value;
-        protected override ResultCore<T> DoParse(Reader input)
+        readonly TResult _value;
+        protected override ResultCore<TResult> DoParse(Reader input)
         {
+            // 元になったパーサーでパースを実施、
+            // 結果型の変換のみ行って呼び出し元に返す
             return _original.Parse(input).Typed(_value);
         }
         public override string ToString()
--- Unclazz.Parsec/CoreParsers/UntypedParser.cs
+++ Unclazz.Parsec/CoreParsers/UntypedParser.cs
@@ -2,6 +2,11 @@
 
 namespace Unclazz.Parsec.CoreParsers
 {
+    /// <summary>
+    /// 結果型を持つパーサーから結果型を取り除くパーサーです。
+    /// 元になったパーサーのキャプチャ結果は破棄されます。
+    /// </summary>
+    /// <typeparam name="TSource">元になったパーサーの結果型</typeparam>
     sealed class UntypedParser<TSource> : Parser
     {
         internal UntypedParser(Parser<TSource> original) : base(original.Configuration)
@@ -11,6 +16,8 @@ namespace Unclazz.Parsec.CoreParsers
         readonly Parser<TSource> _original;
         protected override ResultCore DoParse(Reader input)
         {
+            // 元になったパーサーでパース、
+            // 結果型の変更（剥奪）のみ行って呼び出し元に返す
             return _original.Parse(input).Untyped();
         }
         public override string ToString()
