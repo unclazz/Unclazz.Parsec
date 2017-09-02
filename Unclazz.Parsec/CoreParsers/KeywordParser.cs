1da592bcc43d9ea7a055c4f527007d0f1c6cad70
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-02 10:58:45 +0900

    Keyword/KeywordInのドキュメンテーションXMLを追加

--- Unclazz.Parsec/CoreParsers/KeywordParser.cs
+++ Unclazz.Parsec/CoreParsers/KeywordParser.cs
@@ -4,19 +4,27 @@ using System.Text;
 
 namespace Unclazz.Parsec.CoreParsers
 {
+    /// <summary>
+    /// 指定されたキーワードを読み取るパーサーです。
+    /// カット（Cut）を行う文字位置を指定することができます。
+    /// </summary>
     sealed class KeywordParser : Parser
     {
         internal KeywordParser(string keyword) : this(ParserFactory.Default, keyword) { }
         internal KeywordParser(IParserConfiguration conf, string keyword) : this(conf, keyword, -1) { }
         internal KeywordParser(IParserConfiguration conf, string keyword, int cutIndex) : base(conf)
         {
-            if (cutIndex < -1) throw new ArgumentOutOfRangeException(nameof(cutIndex));
-            _cut = cutIndex;
-            _keyword = keyword ?? throw new ArgumentNullException(nameof(keyword));
-            if (keyword.Length == 0) throw new ArgumentException
-                    ("length of keyword is must be greater than 0.");
-            if (cutIndex == 0 || keyword.Length < cutIndex) throw new ArgumentOutOfRangeException(nameof(cutIndex));
-            if (keyword.Length == cutIndex) _cut = -1;
+            // カットをONにする文字位置（文字境界）は-1以上 かつ 0以外であること
+            if (cutIndex < -1 || cutIndex == 0) throw new ArgumentOutOfRangeException(nameof(cutIndex));
+            // キーワードはnullでないこと
+            if (keyword == null) throw new ArgumentNullException(nameof(keyword));
+            // キーワード文字数は1文字以上であること
+            if (keyword.Length == 0) throw new ArgumentException("length of keyword is must be greater than 0.");
+            // キーワード文字数よりも大きなカット位置は無効
+            if (keyword.Length < cutIndex) throw new ArgumentOutOfRangeException(nameof(cutIndex));
+            // キーワード文字数とカット位置が同一の場合カットはOFF、それ以外の場合指定された位置でカット
+            _cut = keyword.Length == cutIndex ? -1 : cutIndex;
+            _keyword = keyword;
         }
 
         readonly int _cut;
@@ -24,12 +32,19 @@ namespace Unclazz.Parsec.CoreParsers
 
         protected override ResultCore DoParse(Reader input)
         {
+            // キーワードの左側から順番に文字の照合を実施
+            // ＊照合中にEOFに到達した場合も通常の文字と同じ比較処理で検出し
+            // ハンドリングできるため、ループ条件部には何もチェックを入れない
             for (var i = 0; i < _keyword.Length; i++)
             {
+                // 期待される文字
                 var expected = _keyword[i];
+                // 実際の文字
                 var actual = input.Read();
+                // 比較を行う
                 if (expected != actual)
                 {
+                    // NGの場合、その旨の結果を呼び出し元に返す
                     return Failure(string.Format("expected {0} but found {1} at index {2} in \"{3}\"",
                         ParsecUtility.CharToString(expected), ParsecUtility.CharToString(actual), i, _keyword),
                         -1 == _cut || i < _cut);
