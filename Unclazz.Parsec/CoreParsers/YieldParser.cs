8b87d3f82da450d9edd2ebf2ce4b21c36918bac3
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-02 09:46:42 +0900

    YieldにドキュメンテーションXMLを追加

--- Unclazz.Parsec/CoreParsers/YieldParser.cs
+++ Unclazz.Parsec/CoreParsers/YieldParser.cs
@@ -1,20 +1,24 @@
 ﻿namespace Unclazz.Parsec.CoreParsers
 {
-    sealed class YieldParser<T> : Parser<T>
+    /// <summary>
+    /// いついかなるときも指定された値を返すパーサーです。
+    /// このパーサーはいつでもパース成功の結果を返します。
+    /// パースにあたって文字位置の変更を行いません。
+    /// </summary>
+    /// <typeparam name="TResult">このパーサーの結果型</typeparam>
+    sealed class YieldParser<TResult> : Parser<TResult>
     {
-        internal YieldParser(T value) : this(ParserFactory.Default, value) { }
-        internal YieldParser(IParserConfiguration conf, T value) : base(conf)
+        internal YieldParser(TResult value) : this(ParserFactory.Default, value) { }
+        internal YieldParser(IParserConfiguration conf, TResult value) : base(conf)
         {
             _value = value;
         }
-
-        readonly T _value;
-
-        protected override ResultCore<T> DoParse(Reader input)
+        readonly TResult _value;
+        protected override ResultCore<TResult> DoParse(Reader input)
         {
+            // いつでも成功
             return Success(_value);
         }
-
         public override string ToString()
         {
             return string.Format("Yield({0})");
