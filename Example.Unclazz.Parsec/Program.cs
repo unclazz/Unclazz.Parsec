d274ac99d20d4a8118a754706420dd0f2eebdf69
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-02 18:37:46 +0900

    example.exeでJSONがパースされるようコードを復活

--- Example.Unclazz.Parsec/Program.cs
+++ Example.Unclazz.Parsec/Program.cs
@@ -11,6 +11,13 @@ namespace Example.Unclazz.Parsec
     {
         static void Main(string[] args)
         {
+            var parser = new ExpressionParser();
+            var input = args.Aggregate(new StringBuilder(),
+                (b, a) => b.Append(a).Append(' ')).ToString();
+
+            parser.Parse(input)
+                .IfSuccessful(c => Console.WriteLine("result = {0}", c.Value), 
+                m => Console.WriteLine("error = {0}", m));
         }
     }
 }
