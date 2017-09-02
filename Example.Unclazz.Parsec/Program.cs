bef4aa143624e544108e4c41ed0e7224c44fdbef
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-02 18:40:11 +0900

    example.exeで四則演算がパースされるようコードを復活

--- Example.Unclazz.Parsec/Program.cs
+++ Example.Unclazz.Parsec/Program.cs
@@ -11,6 +11,13 @@ namespace Example.Unclazz.Parsec
     {
         static void Main(string[] args)
         {
+            var parser = new ExpressionParser();
+            var input = args.Aggregate(new StringBuilder(),
+                (b, a) => b.Append(a).Append(' ')).ToString();
+
+            parser.Parse(input)
+                .IfSuccessful(v => Console.WriteLine("result = {0}", v),
+                                m => Console.WriteLine("error = {0}", m));
         }
     }
 }
