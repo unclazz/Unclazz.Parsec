f133e0d49aa14b2707b298c34dc3cb905b478182
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-17 20:41:38 +0900

    Contextの初期化方式を変更 Context,ContextConfigurerのドキュメンテーションXMLを追加 Contextのロギングの実装を修正

--- Unclazz.Parsec/Context.cs
+++ Unclazz.Parsec/Context.cs
@@ -6,18 +6,32 @@ using System.Threading.Tasks;
 
 namespace Unclazz.Parsec
 {
+    /// <summary>
+    /// パース処理のコンテキストを表すクラスです。
+    /// <para>パーサー開発者に対して入力データソースへのアクセスやロギングAPIを提供します。</para>
+    /// <para>このクラスのインスタンスは<see cref="string"/>や<see cref="Reader"/>からの暗黙キャストのほか、
+    /// <see cref="Reader.ToContext()"/>とその多重定義を通じても取得できます。</para>
+    /// </summary>
     public sealed class Context
     {
+        /// <summary>
+        /// 暗黙のキャストを行います。
+        /// </summary>
+        /// <param name="text"></param>
         public static implicit operator Context(string text)
         {
             return new Context(Reader.From(text));
         }
+        /// <summary>
+        /// 暗黙のキャストを行います。
+        /// </summary>
+        /// <param name="source"></param>
         public static implicit operator Context(Reader source)
         {
             return new Context(source);
         }
 
-        public Context(Reader source) : this(source, null, null, null) { }
+        internal Context(Reader source) : this(source, null, null, null) { }
         internal Context(Reader source, Stack<string> stack, Action<string> logAppender, CharClass skipTarget)
         {
             Source = source ?? throw new ArgumentNullException(nameof(source));
@@ -28,13 +42,37 @@ namespace Unclazz.Parsec
             Logging = logAppender != null;
         }
 
+        /// <summary>
+        /// 入力データソースです。
+        /// </summary>
         public Reader Source { get; }
+        /// <summary>
+        /// ロギングが有効な場合<c>true</c>です。
+        /// </summary>
         public bool Logging { get; }
+        /// <summary>
+        /// 自動スキップが有効な場合<c>true</c>です。
+        /// </summary>
         public bool AutoSkip { get; }
+        /// <summary>
+        /// ロギングで使用されるアペンダーです。
+        /// 未設定の場合（ロギングが無効である場合）、<c>null</c>が返されます。
+        /// </summary>
         internal Action<string> LogAppender { get; }
+        /// <summary>
+        /// 自動スキップで使用されるアペンダーです。
+        /// 未設定の場合（自動スキップが無効である場合）、<c>null</c>が返されます。
+        /// </summary>
         internal CharClass SkipTarget { get; }
+        /// <summary>
+        /// パーサーの呼び出しスタックです。
+        /// </summary>
         internal Stack<string> Stack { get; }
 
+        /// <summary>
+        /// ログを書き出します。
+        /// </summary>
+        /// <param name="message"></param>
         public void Log(string message)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -42,6 +80,11 @@ namespace Unclazz.Parsec
             // 存在する場合は文字列を作成してアペンダーをコール
             LogAppender(MakeLabel(' ').Append(message).ToString());
         }
+        /// <summary>
+        /// ログを書き出します。
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="arg0"></param>
         public void Log(string format, object arg0)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -49,6 +92,12 @@ namespace Unclazz.Parsec
             // 存在する場合はメッセージを作成してLog(string)をコール
             Log(string.Format(format, arg0));
         }
+        /// <summary>
+        /// ログを書き出します。
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="arg0"></param>
+        /// <param name="arg1"></param>
         public void Log(string format, object arg0, object arg1)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -56,6 +105,13 @@ namespace Unclazz.Parsec
             // 存在する場合はメッセージを作成してLog(string)をコール
             Log(string.Format(format, arg0, arg1));
         }
+        /// <summary>
+        /// ログを書き出します。
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="arg0"></param>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
         public void Log(string format, object arg0, object arg1, object arg2)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -63,6 +119,11 @@ namespace Unclazz.Parsec
             // 存在する場合はメッセージを作成してLog(string)をコール
             Log(string.Format(format, arg0, arg1, arg2));
         }
+        /// <summary>
+        /// ログを書き出します。
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
         public void Log(string format, params object[] args)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -70,12 +131,25 @@ namespace Unclazz.Parsec
             // 存在する場合はメッセージを作成してLog(string)をコール
             Log(string.Format(format, args));
         }
-        public Context Configure(Action<ContextConfigurer> ctxConfig)
+        /// <summary>
+        /// コンテキストのコンフィギュレーションを変更します。
+        /// <para>
+        /// このメソッドは新しいコンテキストのインスタンスを生成して返します。
+        /// もとのインスタンスは変更されません。
+        /// </para>
+        /// </summary>
+        /// <param name="act"></param>
+        /// <returns></returns>
+        public Context Configure(Action<ContextConfigurer> act)
         {
             var config = new ContextConfigurer(this);
-            ctxConfig(config);
+            act(config);
             return config.Make();
         }
+        /// <summary>
+        /// パース本処理前に呼び出しスタックを更新し、自動スキップとロギングを行います。
+        /// </summary>
+        /// <param name="parserName"></param>
         internal void PreParse(string parserName)
         {
             // 自動スキップを試行
@@ -87,6 +161,9 @@ namespace Unclazz.Parsec
             // パース開始を示すログメッセージを作成してアペンダーをコール
             LogAppender(MakeLabel('+').ToString());
         }
+        /// <summary>
+        /// 自動スキップを行います。
+        /// </summary>
         internal void DoSkip()
         {
             // スキップ対象が存在しない場合は直ちに処理を終了
@@ -99,6 +176,10 @@ namespace Unclazz.Parsec
                 Source.Read();
             }
         }
+        /// <summary>
+        /// パース本処理後にロギングを行い、呼び出しスタックを更新します。
+        /// </summary>
+        /// <param name="result"></param>
         internal void PostParse(ResultCore result)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -114,6 +195,10 @@ namespace Unclazz.Parsec
             // スタックから直近のパーサー名を削除
             Stack.Pop();
         }
+        /// <summary>
+        /// パース本処理後にロギングを行い、呼び出しスタックを更新します。
+        /// </summary>
+        /// <param name="result"></param>
         internal void PostParse<T>(ResultCore<T> result)
         {
             // アペンダーが存在しない場合は処理を直ちに終了
@@ -133,39 +218,10 @@ namespace Unclazz.Parsec
         {
             var pos = Source.Position;
             return new StringBuilder()
-                .Append(' ', Stack.Count * 2)
+                .Append(' ', (Stack.Count -1) * 2)
                 .Append(sign).Append(' ').Append(Stack.Peek())
                 .Append(" (ln=").Append(pos.Line).Append(", col=")
                 .Append(pos.Column).Append(", idx=").Append(pos.Index).Append(") ");
-        }
-    }
-
-    public sealed class ContextConfigurer
-    {
-        internal ContextConfigurer(Context orig)
-        {
-            _orig = orig ?? throw new ArgumentNullException(nameof(orig));
-            _skipTarget = orig.SkipTarget;
-            _logAppender = orig.LogAppender;
-        }
-
-        readonly Context _orig;
-        CharClass _skipTarget;
-        Action<string> _logAppender;
-
-        public ContextConfigurer SetLogAppender(Action<string> act)
-        {
-            _logAppender = act;
-            return this;
-        }
-        public ContextConfigurer SetSkipTarget(CharClass clazz)
-        {
-            _skipTarget = clazz;
-            return this;
-        }
-        internal Context Make()
-        {
-            return new Context(_orig.Source, _orig.Stack, _logAppender, _skipTarget);
         }
     }
 }
---
+++ Unclazz.Parsec/ContextConfigurer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Unclazz.Parsec
+{
+    /// <summary>
+    /// <see cref="Context"/>のコンフィギュレーションを変更するためのクラスです。
+    /// </summary>
+    public sealed class ContextConfigurer
+    {
+        internal ContextConfigurer(Context orig)
+        {
+            _orig = orig ?? throw new ArgumentNullException(nameof(orig));
+            _skipTarget = orig.SkipTarget;
+            _logAppender = orig.LogAppender;
+        }
+
+        readonly Context _orig;
+        CharClass _skipTarget;
+        Action<string> _logAppender;
+
+        /// <summary>
+        /// ログ出力で使用されるアペンダーを設定します。
+        /// <para>ログを無効化する場合は<c>null</c>を指定します。</para>
+        /// </summary>
+        /// <param name="act"></param>
+        /// <returns></returns>
+        public ContextConfigurer SetLogAppender(Action<string> act)
+        {
+            _logAppender = act;
+            return this;
+        }
+        /// <summary>
+        /// 自動スキップの対象となる文字クラスを設定します。
+        /// <para>自動スキップを無効化する場合は<c>null</c>を指定します。</para>
+        /// </summary>
+        /// <param name="clazz"></param>
+        /// <returns></returns>
+        public ContextConfigurer SetSkipTarget(CharClass clazz)
+        {
+            _skipTarget = clazz;
+            return this;
+        }
+        internal Context Make()
+        {
+            return new Context(_orig.Source, _orig.Stack, _logAppender, _skipTarget);
+        }
+    }
+}
