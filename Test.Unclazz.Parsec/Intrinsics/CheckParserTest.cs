c823f3da64ad8c423ac792fa093325123411c856
mizuky fujitani <mizuki.fujitani@gmail.com>
2018-06-03 19:19:55 +0900

    CheckParser<T>を追加

---
+++ Test.Unclazz.Parsec/Intrinsics/CheckParserTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using Unclazz.Parsec;
+
+namespace Test.Unclazz.Parsec.Intrinsics
+{
+    [TestFixture]
+    public class CheckParserTest : ParserBase
+    {
+        [Test]
+        public void Parse_Case1()
+        {
+            // Arrange
+            var p = CharsWhileIn(CharClass.Alphabetic).Capture().Check(a => a != "hello", "ng");
+
+            // Act
+            var r = p.Parse("hello ...");
+
+            // Assert
+            Assert.That(r.Successful, Is.False);
+            Assert.That(r.Message, Is.EqualTo("ng"));
+            Assert.That(r.Start.Index, Is.EqualTo(0));
+            Assert.That(r.End.Index, Is.EqualTo(5));
+        }
+        [Test]
+        public void Parse_Case2()
+        {
+            // Arrange
+            var p = CharsWhileIn(CharClass.Alphabetic).Capture().Check(a => a != "hello", "ng");
+
+            // Act
+            var r = p.Parse("hallo ...");
+
+            // Assert
+            Assert.That(r.Successful, Is.True);
+            Assert.That(r.Capture, Is.EqualTo("hallo"));
+            Assert.That(r.Start.Index, Is.EqualTo(0));
+            Assert.That(r.End.Index, Is.EqualTo(5));
+        }
+        [Test]
+        public void Parse_Case3()
+        {
+            // Arrange
+            var p = CharsWhileIn(CharClass.Alphabetic).Capture().Check(a => a != "hello", "ng");
+
+            // Act
+            var r = p.Parse("_hello ...");
+
+            // Assert
+            Assert.That(r.Successful, Is.False);
+            Assert.That(r.Message, Is.Not.EqualTo("ng"));
+            Assert.That(r.Start.Index, Is.EqualTo(0));
+            Assert.That(r.End.Index, Is.EqualTo(0));
+        }
+    }
+}
---
+++ Unclazz.Parsec/Intrinsics/CheckParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Unclazz.Parsec.Intrinsics
+{
+    sealed class CheckParser<T> : Parser<T>
+    {
+        internal CheckParser(Parser<T> parser, Func<T, bool> check, string message)
+        {
+            _parser = parser ?? throw new ArgumentNullException(nameof(parser));
+            _check = check ?? throw new ArgumentNullException(nameof(check));
+            _message = message ?? throw new ArgumentNullException(nameof(message));
+        }
+
+        readonly Parser<T> _parser;
+        readonly Func<T, bool> _check;
+        readonly string _message;
+
+        protected override ResultCore<T> DoParse(Reader src)
+        {
+            // 元になったパーサーのパース結果を得る。
+            var result = _parser.Parse(src);
+
+            // パース成功の場合、チェックを実施する。
+            // その結果がNGならこのパーサーのパース結果も失敗とする。
+            // それ以外の場合（元のパーサーのパース結果が成功でチェック結果もOKの場合、
+            // もしくは、元のパーサーのパース結果が失敗の場合）は元のパーサーのパース結果をそのまま返す。
+            return result.Successful && !_check(result.Capture) ? Failure(_message) : result;
+        }
+    }
+}
